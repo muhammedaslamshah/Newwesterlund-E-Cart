5ca7c5d81e8b11567f554b9b0f97a7541bb95e0e
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-01-15 03:17:56 +0530

    5th commit | Account Management | Register | Login | Exception Handling | Newwesterlund E-com

--- ecart/customers/views.py
+++ ecart/customers/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+#User model
+from django.contrib.auth.models import User
+
+# Messages 
+from django.contrib import messages
+
+#Customer model
+from . models import Customer
 
 # Create your views here.
 def show_account(request):
-    return render(request, 'account.html')
+
+    if request.POST and 'register' in request.POST:
+        try:
+            username=request.POST.get('username')
+            password=request.POST.get('password')
+            email=request.POST.get('email')
+            address=request.POST.get('address')
+            phone=request.POST.get('phone')
+
+            # Create user objects to user account
+            user = User.objects.create(
+                username=username,
+                password=password,
+                email=email
+            )
+            # Create Customer object to Customer account (Customer Profile)
+            customer = Customer.objects.create(
+                user=user,
+                phone=phone,
+                address=address
+            )
+            return redirect('home')
+        except Exception as e: 
+            error_message="username already exists or invalid credentials"
+            # to render the message to template
+            messages.error(request,error_message)
+    return render(request,'account.html')
