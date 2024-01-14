1e2f36893fcf723721ff21260dece03ccfcc77e7
muhammedaslamshah <muhammedaslamshah700@gmail.com>
2024-01-15 00:45:56 +0530

    4th commit | Products Page Finish | Featured Products | Latest Products | & Home Page Finish | User Management

--- ecart/products/views.py
+++ ecart/products/views.py
@@ -3,8 +3,14 @@ from . models import Product
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
-
+    featured_products=Product.objects.order_by('priority')[:4]
+    latest_products=Product.objects.order_by('-id')[:4]
+    context={
+        'featured_products' :featured_products,
+        'latest_products' :latest_products
+    }
+    return render(request,'index.html',context)
+    
 def list_products(request):
     """_summary_
     returns prouduct list page
@@ -17,13 +23,13 @@ def list_products(request):
     page=1
     if request.GET:
         page=request.GET.get('page',1)
-    product_list=Product.objects.all()
-    product_paginator=Paginator(product_list,1)
+    product_list=Product.objects.order_by('priority') #priority reverse order
+    product_paginator=Paginator(product_list,2)
     product_list=product_paginator.get_page(page)
     context={'products':product_list}
     return render(request,'products.html', context)
 
-def detail_product(request):
-    if request.POST:
-        print(request.POST)
-    return render(request,'product_detail.html')
+def detail_product(request,pk):
+    product=Product.objects.get(pk=pk)
+    context={'product':product}
+    return render(request,'product_detail.html',context)
